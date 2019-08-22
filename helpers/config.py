f9f43e00b5ee7cd012c5f0bc703448e9a2323b5d
Евсюков Максим (m.evsyukov) <m.evsyukov@2gis.ru>
2019-08-23 02:14:23 +0700

    Third

--- helpers/config.py
+++ helpers/config.py
@@ -1,16 +1,13 @@
-@property
 def base_url():
     url = "http://165.22.81.75:8090"
-    return url
+    return str(url)
 
 
-@property
 def calculate_method():
     url = "/test/calculate"
-    return url
+    return str(url)
 
 
-@property
 def api_url():
-    url = base_url + calculate_method
-    return url
+    url = base_url() + calculate_method()
+    return str(url)
